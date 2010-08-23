81e48d55df4b291e65bd894b88f206914c170c6e
Stephen Hicks <sdh33@cornell.edu>
2010-08-22 20:42:05 -0700

    add .gitignore update.sh sheets/Makefile

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Updates the github page with current PDFs.
+
+if [ -n "$(git diff --name-only)" ]; then
+  echo "Please commit changes!"
+  exit 1
+fi
+
+FILES="system.pdf basic.pdf competitive.pdf points.pdf sheets.pdf"
+make all
+mv sheets/sheets.pdf sheets.pdf
+for FILE in $FILES; do
+    mv $FILE $FILE.backup
+done
+git checkout gh-pages
+for FILE in $FILES; do
+    mv $FILE.backup $FILE
+done
+git commit -am 'update PDFs'
+git push origin
+git checkout master
+rm $FILES
